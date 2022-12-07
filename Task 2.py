3e7d30597e9860dfcbd15fe9672d45bcba05f042
Mikhail <mehan@list.ru>
2022-12-07 21:25:04 +0300

    Done

---
+++ Task 2.py
@@ -0,0 +1,20 @@
+# Задайте натуральное число N. Напишите программу, которая составит список простых множителей числа N.
+
+
+def factorize(n):
+    factors = []
+    i = 2
+    while i * i <= n:
+        while n % i == 0:
+            n //= i
+            factors.append(i)
+        i += 1
+    if n > 1:
+        factors.append(n)
+    return factors
+
+
+num = int(input("Введите число: "))
+
+nums = factorize(num)
+print(f"Простыми множителями числа {num} являются числа {nums}")
