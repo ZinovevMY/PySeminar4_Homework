ed72e17a3d20cdf401cad99e221e22ac7bdf3ff3
Mikhail Zinovev <mehan@list.ru>
2022-12-07 11:33:25 +0300

    Lambda realisation

--- Task 1.py
+++ Task 1.py
@@ -3,11 +3,7 @@
 import math
 
 
-def truncate(number: float, digits: int):
-    pow10 = 10 ** digits
-    return number * pow10 // 1 / pow10
-
-
 precision = int(input("Введите количество знаков после запятой: "))
-res_pi = truncate(math.pi, precision)
-print(f"Число ПИ с точнойстью {precision} знака равно {res_pi}")
+pow10 = 10 ** precision
+res_pi = lambda x: x * pow10 // 1 / pow10
+print(f"Число ПИ с точнойстью {precision} знака равно {res_pi(math.pi)}")
